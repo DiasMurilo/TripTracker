c9d174c0a3b4f6a7f1f1664452042b55fc2b10fb
Murilo Dias <dias.murilo@gmail.com>
2021-03-14 13:55:29 +0000

    5th Commit. Changes:
    Added:
    - firebase dependencies
    - JSON document
    
    screen deleted  (error)
    - 6_TRIP_FINISHED

--- app/src/main/java/com/example/triptracker/MainActivity.java
+++ app/src/main/java/com/example/triptracker/MainActivity.java
@@ -1,31 +1,62 @@
 package com.example.triptracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
-    private FirebaseAuth mAuth;
+
+    Button mButtonLogin;
+
+    //private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
-        mAuth = FirebaseAuth.getInstance();
+
+
+        //mAuth = FirebaseAuth.getInstance();
+        mButtonLogin = findViewById(R.id.buttonLogin);
+
+        mButtonLogin.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Authentication failed.",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
-    @Override
+
+
+
+
+
+
+
+    /*@Override
     public void onStart() {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
         updateUI(currentUser);
     }
+
+
+
 
 
 // Create a new createAccount method which takes in an email address and password, validates them and then creates a new user with the createUserWithEmailAndPassword method.
@@ -48,7 +79,7 @@ mAuth.createUserWithEmailAndPassword(email, password)
 
             // ...
         }
-    });
+    });*/
 
 
 
